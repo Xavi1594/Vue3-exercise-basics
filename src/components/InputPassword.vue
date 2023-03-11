5328d9e2a2884a8d3a40876e312d2aabca20bba6
Xavi1594 <xavi9415@hotmail.com>
2023-03-11 13:15:53 +0100

    ejercicio acabado

---
+++ src/components/InputPassword.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, computed } from 'vue';
+
+const isPasswordVisible = ref(false);
+
+const inputType = computed(() => {
+  return isPasswordVisible.value ? "text" : "password";
+});
+
+const buttonText = computed(() => {
+  return isPasswordVisible.value ? "Hide Password" : "Show Password";
+});
+
+function showPassword() {
+  isPasswordVisible.value = !isPasswordVisible.value;
+}
+</script>
+
+<template>
+  <div class="container text-center input-password">
+    <h1 class="my-3">Exercise Input Password</h1>
+    <input :type="inputType" name="" id="password" class="form-control">
+    <div class="d-block">
+      <span id="show-password" class="mt-2 btn btn-primary" @click="showPassword()">
+        {{ buttonText }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.input-password {
+  max-width: 500px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/ReadMore.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from "vue";
+
+const title = ref("Exercise Read More");
+const isActive = ref(false);
+
+const mostrarTodo = () => {
+  isActive.value = !isActive.value;
+};
+</script>
+
+<template>
+  <div class="container text-center">
+    <h1 class="my-3">{{ title }}</h1>
+    <div>
+      <p class="mb-3 d-block" :class="{ 'cut-text': !isActive }">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos
+        ipsa neque odio consectetur impedit tempore modi accusantium! Voluptatem
+        molestiae nemo inventore soluta corrupti. Culpa perferendis sapiente
+        deserunt aperiam maxime consectetur recusandae quasi commodi dicta
+        temporibus pariatur ab repellendus dignissimos natus aliquam odit ullam,
+        quaerat autem non hic, quidem dolore laboriosam.
+      </p>
+      <button @click="mostrarTodo" class="btn btn-primary">
+        {{ isActive ? "Show Less" : "Show More" }}
+      </button>
+    </div>
+    <hr class="mb-4" />
+  </div>
+</template>
+
+
+
+<style lang="css"></style>
